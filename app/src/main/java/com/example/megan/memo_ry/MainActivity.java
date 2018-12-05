b86dd195deb0392fc3a6aa88335813c63decd7b9
Microsoft <rachelbmorton@yahoo.com>
2018-12-04 19:18:37 -0500

    finished video 4

--- app/src/main/java/com/example/megan/memo_ry/MainActivity.java
+++ app/src/main/java/com/example/megan/memo_ry/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.megan.memo_ry;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +13,21 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    PagerTabStrip tabs;
+    ViewPager viewPager;
+
+
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.pager);
+        tabs = (PagerTabStrip)findViewById(R.id.tabStrip);
+
+        viewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
 
         button = (Button) findViewById(R.id.buttonCreateNew);
         button. setOnClickListener(new View.OnClickListener() {
@@ -30,6 +44,42 @@ public class MainActivity extends AppCompatActivity {
                 openDiary();
             }
         });
+    }
+
+    public class MyAdapter extends FragmentPagerAdapter
+    {
+        String [] titles = ("Record","Saved Recordings");
+        public MyAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+        @Override
+        public Fragment getItem (int position)
+        {
+            switch(position)
+            {
+                case 0: return RecordFragment.newInstance(position);
+                case 1: return FileViwerFragment.newInstance(position);
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount()
+        {
+            return titles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            return titles(position);
+        }
+    }
+
+    public MainActivity ()
+    {
+        
     }
 
     public void openNewMemory () {
